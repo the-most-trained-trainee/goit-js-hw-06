41588479a610b31b9eb9950538a07ab5d9df3600
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-23 17:16:55 +0300

    Interim Commit 23072022 (task 10)

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// My code below
+
+const divQuantity = document.querySelector("#controls > input");
+const calculateButtons = document.querySelectorAll("button");
+const createButton = calculateButtons[0];
+const destroyButton = calculateButtons[1];
+const boxesContainer = document.querySelector("#boxes");
+let divInclusion = [];
+
+
+createButton.addEventListener("click", () => { 
+
+  for (let i = 0; i < divQuantity.value; i++) {
+    let div = document.createElement("div");
+    div.style.width = `${i * 10 + 30}px`;
+    div.style.background = getRandomHexColor();
+    divInclusion.push(div);
+  }
+  console.log(divInclusion)
+
+})
