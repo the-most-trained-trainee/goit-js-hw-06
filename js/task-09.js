ec048ae6f22be1ddb91266e85a05fcbfd4866a8f
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-23 16:18:20 +0300

    Interim Commit 23072022 (task 9)

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// My code below 
+
+const changeColorButton = document.querySelector(".change-color");
+const pageBody = document.querySelector("body");
+const colorDisplay = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", () => { 
+  let currentColor = getRandomHexColor();
+  pageBody.style.background = currentColor;
+  colorDisplay.textContent = currentColor;
+})
