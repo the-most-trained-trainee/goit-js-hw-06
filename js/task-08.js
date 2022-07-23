c950ddef10f44433e230b1a227e1494787297b32
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-23 16:08:07 +0300

    Interim Commit 23072022 (task 8)

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+// My code below 
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", onSubmit);
+
+function onSubmit(event) { 
+
+    event.preventDefault();
+
+    if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
+        alert("Please, fill all the fields");
+    } else { 
+        const formData = new FormData(event.currentTarget);
+        let formOutPut = {};
+        
+        formData.forEach((value, name) => {
+                formOutPut[name] = value;
+            });
+        event.currentTarget.reset();
+        console.log(formOutPut)
+    }
+}
